4cc5afe4669454bc21a77c0a6911d8300caa8c29
BastaMasta <56192822+BastaMasta@users.noreply.github.com>
2023-11-16 13:45:33 +0530

    added main encryption key making logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+use rand::Rng;
+
 fn main() {
     let chars_vec : Vec<char> = vec!['0','1','2','3','4','5','6','7','8','9','A','B','C','D',
                                          'E','F','H','I','J','K','L','M','N','O','P','Q','R','S',
@@ -10,8 +13,16 @@ fn main() {
                                          '¶','¸','¹','º','»','¿','×','ß','æ','ø','þ','Ɔ','Ɣ','ƕ',
                                          'Ɯ','ƛ','Ƨ','Ʃ','ƪ','Ʊ','ƿ','ȸ','ʬ','ʮ','ʪ','ʩ','ʢ','ʡ',
                                          'ʝ'];
-    for i in 0..5{
+    for num in 0..5{
         let mut key_vec = chars_vec.clone();
         let mut val_vec = chars_vec.clone();
+        let mut encr_hash : HashMap<char, char> = HashMap::new();
+        for i in key_vec {
+            let index = rand::thread_rng().gen_range(0..val_vec.len());
+            encr_hash.insert(i.clone(), val_vec[index].clone());
+            val_vec.remove(index);
+        }
+        println!("{:?}", encr_hash);
+        println!("\n\n\n\n\n\n\n")
     }
 }
